c66d7bff13538d2bddb5c0966597b2fb509e522e
Chandra Shekhar <shekharindian099@gmail.com>
2017-04-04 17:09:27 +0530

    remove store

--- src/app/app.state.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectState } from './project/reducers/project.state';
-// This should hold the AppState interface
-// Ideally importing all the substate for the application
-
-export interface AppState {
-  projects: ProjectState;
-}

acda727f1393fb9d5a64a10319219e865669298d
Chandra Shekhar <shekharindian099@gmail.com>
2017-04-10 15:33:35 +0530

    remove auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', loadChildren: './home/home.module#HomeModule' },
-  { path: 'projects', loadChildren: './project/project.module#ProjectModule'},
-  { path: 'auth', loadChildren: './auth/auth.module#AuthModule' }
+  { path: 'projects', loadChildren: './project/project.module#ProjectModule'}
 ];

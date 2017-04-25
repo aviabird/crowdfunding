8ed8c0f234cdd530369aee58d3acd1392150ac70
Chandra Shekhar <shekharindian099@gmail.com>
2017-04-25 15:23:32 +0530

    add new design css

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -4,7 +4,7 @@ import { CustomConfig } from 'ng2-ui-auth';
 
 const apiLink: string = environment.API_ENDPOINT; // "http://localhost:3000";
 export const GOOGLE_CLIENT_ID = '676583033944-57tm364opa3o3deln53gkg3g46r4j3gh.apps.googleusercontent.com';
-export const FACEBOOK_CLIENT_ID = '763807900464091';
+export const FACEBOOK_CLIENT_ID = '801579219992920';
 
 export class MyAuthConfig extends CustomConfig {
   defaultHeaders = { 'Content-Type': 'application/json' };

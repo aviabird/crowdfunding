5daf17ae1249b813cccca0bd8a9b9f1053db215d
Chandra Shekhar <shekharindian099@gmail.com>
2017-05-12 14:18:06 +0530

    Added validation on reward amount and removed double share button on project detail

--- src/app/project/components/project-detail/pledge-more/pledge-more.component.ts
+++ src/app/project/components/project-detail/pledge-more/pledge-more.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { Reward } from './../../../../core/models/reward';
 import { DateService } from './../../../../core/services/date.service';
 import { Project } from './../../../../core/models/project';
 import { Component, OnInit, Input } from '@angular/core';
@@ -11,10 +13,27 @@ export class PledgeMoreComponent implements OnInit {
 
   amount: number;
   @Input() project: Project;
+  isAmountValid: boolean;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.isAmountValid = true;
+  }
 
   ngOnInit() {
+  }
+
+  onContinue(index) {
+    const reward: Reward = this.project.rewards[index];
+    this.isAmountValid = this.amount < reward.amount || typeof this.amount === 'undefined'  ? false : true;
+
+    if (this.isAmountValid) {
+      this.router.navigate(['/projects', this.project.id, 'payment'], {
+        queryParams: {
+          'amount': reward.amount,
+          'reward': reward.id
+        }
+      });
+    }
   }
 
 }

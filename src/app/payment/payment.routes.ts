9eec32d18fdc5ccd31744954a1642bbf57104496
Chandra Shekhar <shekharindian099@gmail.com>
2017-05-12 20:39:36 +0530

    add auth guard to payment component

--- src/app/payment/payment.routes.ts
+++ src/app/payment/payment.routes.ts
@@ -1,8 +1,9 @@
+import { AuthGuard } from './../core/guards/auth.guard';
 import { SofortRedirectUrlComponent } from './sofort-redirect-url/sofort-redirect-url.component';
 import { PaymentComponent } from './payment.component';
 import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', component: PaymentComponent },
+  { path: '', component: PaymentComponent, canActivate: [AuthGuard] },
   { path: 'sofort/redirect', component: SofortRedirectUrlComponent }
 ];
